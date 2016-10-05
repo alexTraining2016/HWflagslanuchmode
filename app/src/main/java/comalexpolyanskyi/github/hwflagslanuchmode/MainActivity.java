e7154a175b2c280a15934632151bc40ab80fd7ad
AlexPolyanskyi <lol.www@tut.by>
2016-10-06 01:53:01 +0300

    add toolbar avatar and name

--- app/src/main/java/comalexpolyanskyi/github/hwflagslanuchmode/MainActivity.java
+++ app/src/main/java/comalexpolyanskyi/github/hwflagslanuchmode/MainActivity.java
@@ -1,12 +1,28 @@
 package comalexpolyanskyi.github.hwflagslanuchmode;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Alex Alexeev");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
